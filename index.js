9edb1e31cab694aa4f76ab55b1d523d16ac6425a
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-05-20 00:30:34 +0200

    Read config from command argument

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const makeFrontendApp = require("nflix");
 const makeApplication = require("webtorrent-api");
-const appConfig = require("./config.json");
+const appConfig = require(process.argv[2]);
 
 const frontendApp = makeFrontendApp(appConfig["nflix"]);
 const app = makeApplication(appConfig["webtorrent-api"]);
-
 Promise.all([app.start(), frontendApp.start()]);
 
 process.on("SIGINT", async function () {
