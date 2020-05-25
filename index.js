54b3364555c0d53aa0d323fd04203ec39fb5120e
Zoltan Kakuk <kakuk.zoltan@e-vista.hu>
2020-05-25 02:06:52 +0200

    Upgrade webtorrent-api and handle sigterm event

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const frontendApp = makeFrontendApp(appConfig["nflix"]);
 const app = makeApplication(appConfig["webtorrent-api"]);
 Promise.all([app.start(), frontendApp.start()]);
 
-process.on("SIGINT", async function () {
-  await Promise.all([app.stop(), frontendApp.stop()]);
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+async function shutdown() {
+  await frontendApp.stop();
+  await app.stop();
   console.log("Bye");
-});
+}
